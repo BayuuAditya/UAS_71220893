53d3ff9adb3687036efaf79c2a6c05870f6fb10b
BayuuAditya <117901006+BayuuAditya@users.noreply.github.com>
2023-01-03 14:39:29 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,134 @@
+print("======================")
+print("****Justice League****")
+print("======================")
+user = (input("Masukan Username Anda: "))
+
+if user ==  "victorstone":
+    print("====WELCOME Victor Stone====")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if user ==  "brucewayne":
+    print("====WELCOME Bruce Wayne====")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if user ==  "ciscoramon":
+    print("====WELCOME Ciscoramon====")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if user == "romanoff":   
+    print("Acces Denied")
+    print("Anggota Avengers Dilarang Masuk")
+    print(" 4. Exit")
+#1
+p = input("Masukan Pilihan Anda :")
+if p == "1" : 
+    r = input("Nama Anggota Baru : ")
+    print ("Data", r, "Berhasil Ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+    
+if p == "2" :
+    r = input("Anggota yang akan dihapus :")
+    print("Data", r,"Berhasil Dihapus")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+    
+if p == "3" : 
+    print("Daftar Anggota Justice League :")
+    print("Belum Ada" )
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+
+if p == "4" :
+    print("see u next time MR.", user, "GLHF")
+    
+
+#2
+p = input("Masukan Pilihan Anda :")
+if p == "1" : 
+    w = input("Nama Anggota Baru : ")
+    print ("Data", w, "Berhasil Ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "2" : 
+    w = input("Anggota yang akan dihapus: ")
+    print("Data", w,"Berhasil Dihapus")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "3" : 
+    print("Daftar Anggota Justice League :")
+    print(r)
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "4" :
+    print("see u next time MR.", user, "GLHF")
+#3
+p = input("Masukan Pilihan Anda :")
+if p == "1" : 
+    r = input("Nama Anggota Baru : ")
+    print ("Data", r, "Berhasil Ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "2" : 
+    r = input("Anggota yang akan dihapus: ")
+    print("Data", r,"Berhasil Dihapus")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "3" : 
+    print("Daftar Anggota Justice League :")
+    print(r)
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "4" :
+    print("see u next time MR.", user, "GLHF")
+#4
+p = input("Masukan Pilihan Anda :")
+if p == "1" : 
+    r = input("Nama Anggota Baru : ")
+    print ("Data", r, "Berhasil Ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "2" : 
+    r = input("Anggota yang akan dihapus: ")
+    print("Data", r,"Berhasil Dihapus")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "3" : 
+    print("Daftar Anggota Justice League :")
+    print(r ,"dan", w)
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus Anggota Justice League")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+if p == "4" :
+    print("see u next time MR.", user, "GLHF")
+
+
